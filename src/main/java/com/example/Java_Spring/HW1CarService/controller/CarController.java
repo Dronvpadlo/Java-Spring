67042e716577dd16910f1a4f9ef4bb8ba0cdfccf
User <doronyuk714@mail.com>
2024-09-28 17:34:33 +0300

    add get, getById, put, post delete, getMaxPowerCar and GetMinPowerCar

--- src/main/java/com/example/Java_Spring/HW1CarService/controller/CarController.java
+++ src/main/java/com/example/Java_Spring/HW1CarService/controller/CarController.java
@@ -4,6 +4,7 @@ import com.example.Java_Spring.HW1CarService.entity.Car;
 import com.example.Java_Spring.HW1CarService.repository.CarRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,19 +15,51 @@ public class CarController {
 
     private final CarRepository carRepository;
     @GetMapping("/cars")
-    public List<Car> getCars(){
-        return carRepository.findAll();
+    public ResponseEntity<List<Car>> getCars(){
+        return ResponseEntity.ok(carRepository.findAll());
+    }
+
+    @GetMapping("/cars/{id}")
+    public ResponseEntity<Car> getCar(@PathVariable Long id){
+        return ResponseEntity.of(carRepository.findById(id));
     }
 
     @PostMapping("/cars")
-    public Car createCar(@RequestBody Car car){
-        return carRepository.save(car);
+    public ResponseEntity<Car> createCar(@RequestBody Car car){
+        return ResponseEntity.ok(carRepository.save(car));
+    }
+
+    @PutMapping("/cars/{id}")
+    public ResponseEntity<Car> changeCar(@PathVariable Long id, @RequestBody Car newCar){
+        return carRepository.findById(id)
+                .map(car -> {
+                    car.setModel(newCar.getModel());
+                    car.setEnginePower(newCar.getEnginePower());
+                    return ResponseEntity.ok(carRepository.save(car));
+                })
+                .orElseGet(()->ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/cars")
-    public List<Car> deleteCar(@RequestBody Car car){
+    public ResponseEntity<List<Car>> deleteCar(@RequestBody Car car){
         carRepository.deleteById(car.getId());
-        return carRepository.findAll();
+        return ResponseEntity.ok(carRepository.findAll());
     }
 
+    @GetMapping("/cars/max-engine-power")
+    public ResponseEntity<Car> foundMaxPowerCar(){
+        return carRepository.findAll()
+                .stream()
+                .max((o1, o2) -> o1.getEnginePower().compareTo(o2.getEnginePower()))
+                .map(ResponseEntity::ok)
+                .orElseGet(()-> ResponseEntity.notFound().build());
+    }
+    @GetMapping("/cars/min-engine-power")
+    public ResponseEntity<Car> foundMinPowerCar(){
+        return carRepository.findAll()
+                .stream()
+                .min(((o1, o2) -> o1.getEnginePower().compareTo(o2.getEnginePower())))
+                .map(ResponseEntity::ok)
+                .orElseGet(()-> ResponseEntity.notFound().build());
+    }
 }
